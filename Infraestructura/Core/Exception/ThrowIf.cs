4c23715f6277deeef39df778e883c30f1cf0fd2a
alex16leiva <alex16leiva@gmail.com>
2024-07-11 07:35:41 -0600

    Se agrego clase de validacion de argumentos

---
+++ Infraestructura/Core/Exception/ThrowIf.cs
@@ -0,0 +1,41 @@
+﻿using Dominio.Core.Extensions;
+
+namespace Infraestructura.Core.Exception
+{
+     /// <summary>
+    /// Exception Helper Class for Parameter Checking.
+    /// </summary>
+    public static class ThrowIf
+    {
+        public static class Argument
+        {
+            /// <summary>
+            /// Argument Null Checking
+            /// </summary>
+            /// <param name="argument">The argument to check.</param>
+            /// <param name="argumentName">The argument's name.</param>
+            public static void IsNull(object argument, string argumentName) 
+            {
+                if (argument.IsNull()) throw new ArgumentNullException(argumentName);
+            }
+
+            /// <summary>
+            /// Argument Null or WhiteSpace Checking
+            /// </summary>
+            /// <param name="argument">The argument to check.</param>
+            /// <param name="argumentName">The argument's name.</param>
+            public static void IsNullOrWhiteSpace(string argument, string argumentName)
+            {
+                if (argument.IsMissingValue()) throw new ArgumentNullException("Argument should not be null or white space", argumentName);
+            }
+
+            public static void IsZeroOrNegative(decimal argument, string argumentName)
+            {
+                if (argument <= 0)
+                {
+                    throw new ArgumentException("Argument should neither zero nor negative", argumentName);
+                }
+            }
+        }
+    }
+}
---
+++ Infraestructura/Core/RestClient/HttpRestClient.cs
@@ -0,0 +1,26 @@
+﻿using Infraestructura.Core.Exception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructura.Core.RestClient
+{
+    public sealed class HttpRestClient
+    {
+        private readonly HttpClient _httpClient;
+
+
+        /// <summary>
+        /// Creates a new instance of <see cref="HttpRestClient"/>.
+        /// </summary>
+        /// <param name="httpClient">The client used internally to consume rest API.</param>
+        public HttpRestClient(HttpClient httpClient)
+        {
+            ThrowIf.Argument.IsNull(httpClient, nameof(httpClient));
+
+            _httpClient = httpClient;
+        }
+    }
+}
